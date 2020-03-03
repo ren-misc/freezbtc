417845474daf66e0288d146eaab4bdd598eca3dd
noiach <gh@noiach.com>
2020-03-03 12:18:14 +1100

    Updates

--- precache-manifest.071faff5b720c2c59b2b5d1c053286c3.js
+++ precache-manifest.071faff5b720c2c59b2b5d1c053286c3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "025d29e2a726dd45f9fa8e4fa2102310",
+    "revision": "56bf9425258afd8c1d6e6aaf331d77ae",
     "url": "/freezbtc/index.html"
   },
   {
-    "revision": "6ecb9a98fab6cb47bd0f",
+    "revision": "7086d0f01d23e9a54227",
     "url": "/freezbtc/static/css/main.7a17a0ab.chunk.css"
   },
   {
-    "revision": "48ce9d4994015070d654",
-    "url": "/freezbtc/static/js/2.8bed5939.chunk.js"
+    "revision": "743223a6df7b2ec6d04b",
+    "url": "/freezbtc/static/js/2.00be7e3b.chunk.js"
   },
   {
     "revision": "eefa402c39a7d49bf58197a0a7cd2ee5",
-    "url": "/freezbtc/static/js/2.8bed5939.chunk.js.LICENSE.txt"
+    "url": "/freezbtc/static/js/2.00be7e3b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6ecb9a98fab6cb47bd0f",
-    "url": "/freezbtc/static/js/main.e27bf090.chunk.js"
+    "revision": "7086d0f01d23e9a54227",
+    "url": "/freezbtc/static/js/main.9c0d06f5.chunk.js"
   },
   {
     "revision": "7a591f230efc39fb7a16",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/freezbtc/precache-manifest.e1828cc1a2f4d3b8155f80644efa83a7.js"
+  "/freezbtc/precache-manifest.071faff5b720c2c59b2b5d1c053286c3.js"
 );
 
 self.addEventListener('message', (event) => {
